8d5bcb748e1caf5c31cdb336f93f247cdb24c20b
Lucas Lindh <lucas.lindh6@live.se>
2026-09-01 15:04:22 +0200

    consumer service

--- src_pipeline/consumer.py
+++ src_pipeline/consumer.py
@@ -8,7 +8,7 @@ def on_message(client, userdata, message):
 
 if __name__ == "__main__":
     client = mqtt.Client()
-    client.connect("locashost", 1883)
+    client.connect("localhost", 1883)
     client.subscribe("home/pico/dht11")
     client.on_message = on_message
     client.loop_forever()
