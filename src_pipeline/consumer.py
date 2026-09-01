45e1d95f36b1b1e875f8e5da8791e95477908888
Lucas Lindh <lucas.lindh6@live.se>
2026-09-01 14:34:15 +0200

    mqtt client

---
+++ src_pipeline/consumer.py
@@ -0,0 +1,14 @@
+import paho.mqtt.client as mqtt
+import json
+
+def on_message(client, userdata, message):
+    payload = message.payload.decode()
+
+    print(payload)
+
+if __name__ == "__main__":
+    client = mqtt.Client()
+    client.connect("locashost", 1883)
+    client.subscribe("home/pico/dht11")
+    client.on_message = on_message
+    client.loop_forever()
