d0e5de3bb187620e3225702a76d40cd139d6d137
Lucas Lindh <lucas.lindh6@live.se>
2026-09-01 11:41:19 +0200

    testing wifi and dht sensor

--- src_pico/main.py
+++ src_pico/main.py
@@ -1,14 +1,24 @@
 from wifi import connect_wifi
 from machine import Pin, PWM
+from dht import DHT11
 
 sound = PWM(Pin(14))
 sound.freq(2700)
 sound.duty_u16(0)
 
 status_led = Pin(15,1)
+dht_sensor = DHT11(Pin(16,1))
 
 print(connect_wifi())
 
 if connect_wifi():
     status_led.value(1)
     sound.duty_u16(100)
+
+while True:
+    dht_sensor.measure()
+    temp = dht_sensor.temperature()
+    humidity = dht_sensor.humidity()
+
+    data = {"temperature": temp, "humidity": humidity}
+    print(data)
